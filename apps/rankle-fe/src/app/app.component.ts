fd6a4bb3acb46ad7c101dc6c56e03704febf97a8
evanfuture <evan@evanpayne.com>
2023-04-26 14:49:56 +0200

    step 2 complete

--- apps/rankle-fe/src/app/app.component.ts
+++ apps/rankle-fe/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ApiClientService } from '@rankle/frontend/utils/api-client';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: "rankle-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  imports: [RouterModule, CommonModule],
+  providers: [ApiClientService],
+  selector: 'rankle-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = "rankle-fe";
+export class AppComponent implements OnInit {
+  helloRes$ = new BehaviorSubject('');
+
+  constructor(private apiClientService: ApiClientService) {}
+
+  async fetchData(): Promise<void> {
+    const res = await this.apiClientService.apiClient.hello.query();
+    this.helloRes$.next(res);
+  }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
 }
